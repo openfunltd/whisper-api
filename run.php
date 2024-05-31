a6e1fa29dd821885c6adfff2999781bdd2ae64ef
Ronny Wang <ronnywang@gmail.com>
2024-05-31 00:30:45 +0000

    log error

--- run.php
+++ run.php
@@ -11,90 +11,96 @@ if ($job->status == 'done') {
     exit;
 }
 
-if ($job->tool == 'whisperx') {
-    $file = JobHelper::getWavFromURL($job->data->url, $logger);
-    $params = '';
-    if ($job->data->diarize) {
-        $params .= ' --diarize';
-    }
-    if ($job->data->language ?? false) {
-        $params .= ' --language ' . escapeshellarg($job->data->language);
+try {
+    if ($job->tool == 'whisperx') {
+        $file = JobHelper::getWavFromURL($job->data->url, $logger);
+        $params = '';
+        if ($job->data->diarize) {
+            $params .= ' --diarize';
+        }
+        if ($job->data->language ?? false) {
+            $params .= ' --language ' . escapeshellarg($job->data->language);
+        } else {
+            $params .= ' --language zh';
+        }
+        if ($job->data->model ?? false) {
+            $params .= ' --model ' . escapeshellarg($job->data->model);
+        } else {
+            $params .= ' --model large-v3';
+        }
+        if ($job->data->init_prompt ?? false) {
+            $params .= ' --initial_prompt ' . escapeshellarg($job->data->init_prompt);
+        }
+
+        $output_dir = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
+        mkdir($output_dir, 0777, true);
+        $logger("running whisperx with $params");
+        $cmd = sprintf("whisperx --output_dir %s %s %s", escapeshellarg($output_dir), $params, escapeshellarg($file));
+
+        system("conda run -n whisperx $cmd", $ret);
+
+        $logger("merging result");
+        $result = new StdClass;
+        foreach (glob("$output_dir/*.*") as $output_file) {
+            $ext = pathinfo($output_file, PATHINFO_EXTENSION);
+            $result->$ext = file_get_contents($output_file);
+        }
+        JobHelper::updateData($job_id, 'result', $result);
+        JobHelper::updateStatus($job_id, 'done');
+    } elseif ('whisper.cpp' == $job->tool) {
+        $file = JobHelper::getWavFromURL($job->data->url, $logger);
+        $output_file = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
+        $whispercpp_dir = getenv('whispercpp_dir');
+        $job->data->language = $job->data->language ?? 'zh';
+        if ($job->data->language ?? false) {
+            $params .= ' --language ' . escapeshellarg($job->data->language);
+        }
+        $job->data->model = $job->data->model ?? 'large-v3';
+        $params .= ' --model ' . escapeshellarg("{$whispercpp_dir}/models/ggml-{$job->data->model}.bin");
+
+        if ($job->data->init_prompt ?? false) {
+            $params .= ' --prompt ' . escapeshellarg($job->data->init_prompt);
+        }
+
+        $logger("running whisper.cpp");
+        $cmd = sprintf("%s %s %s > %s",
+            escapeshellcmd("{$whispercpp_dir}/main"),
+            $params,
+            escapeshellarg($file),
+            escapeshellarg($output_file)
+        );
+        system($cmd, $ret);
+
+        $result = new StdClass;
+        $result->txt = file_get_contents($output_file);
+
+        JobHelper::updateData($job_id, 'result', $result);
+        JobHelper::updateStatus($job_id, 'done');
+    } elseif ('pyannote' == $job->tool) {
+        $file = JobHelper::getWavFromURL($job->data->url, $logger);
+        $output_file = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
+        $pyannote_script = getenv('pyannote_script');
+
+        $logger("running pyannote");
+        $cmd = sprintf("conda run -n pyannote python %s %s > %s",
+            escapeshellarg($pyannote_script),
+            $file,
+            escapeshellarg($output_file)
+        );
+        system($cmd, $ret);
+
+        $result = new StdClass;
+        $result->result = json_decode(file_get_contents($output_file));
+
+        JobHelper::updateData($job_id, 'result', $result);
+        JobHelper::updateStatus($job_id, 'done');
     } else {
-        $params .= ' --language zh';
+        $logger("unknown tool: " . $job->tool);
+        JobHelper::updateStatus($job_id, 'error');
+        exit;
     }
-    if ($job->data->model ?? false) {
-        $params .= ' --model ' . escapeshellarg($job->data->model);
-    } else {
-        $params .= ' --model large-v3';
-    }
-    if ($job->data->init_prompt ?? false) {
-        $params .= ' --initial_prompt ' . escapeshellarg($job->data->init_prompt);
-    }
-
-    $output_dir = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
-    mkdir($output_dir, 0777, true);
-    $logger("running whisperx with $params");
-    $cmd = sprintf("whisperx --output_dir %s %s %s", escapeshellarg($output_dir), $params, escapeshellarg($file));
-
-    system("conda run -n whisperx $cmd", $ret);
-
-    $logger("merging result");
-    $result = new StdClass;
-    foreach (glob("$output_dir/*.*") as $output_file) {
-        $ext = pathinfo($output_file, PATHINFO_EXTENSION);
-        $result->$ext = file_get_contents($output_file);
-    }
-    JobHelper::updateData($job_id, 'result', $result);
-    JobHelper::updateStatus($job_id, 'done');
-} elseif ('whisper.cpp' == $job->tool) {
-    $file = JobHelper::getWavFromURL($job->data->url, $logger);
-    $output_file = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
-    $whispercpp_dir = getenv('whispercpp_dir');
-    $job->data->language = $job->data->language ?? 'zh';
-    if ($job->data->language ?? false) {
-        $params .= ' --language ' . escapeshellarg($job->data->language);
-    }
-    $job->data->model = $job->data->model ?? 'large-v3';
-    $params .= ' --model ' . escapeshellarg("{$whispercpp_dir}/models/ggml-{$job->data->model}.bin");
-
-    if ($job->data->init_prompt ?? false) {
-        $params .= ' --prompt ' . escapeshellarg($job->data->init_prompt);
-    }
-
-    $logger("running whisper.cpp");
-    $cmd = sprintf("%s %s %s > %s",
-        escapeshellcmd("{$whispercpp_dir}/main"),
-        $params,
-        escapeshellarg($file),
-        escapeshellarg($output_file)
-    );
-    system($cmd, $ret);
-
-    $result = new StdClass;
-    $result->txt = file_get_contents($output_file);
-
-    JobHelper::updateData($job_id, 'result', $result);
-    JobHelper::updateStatus($job_id, 'done');
-} elseif ('pyannote' == $job->tool) {
-    $file = JobHelper::getWavFromURL($job->data->url, $logger);
-    $output_file = getenv('data_dir') . '/tmp/' . WebDispatcher::uniqid(12);
-    $pyannote_script = getenv('pyannote_script');
-
-    $logger("running pyannote");
-    $cmd = sprintf("conda run -n pyannote python %s %s > %s",
-        escapeshellarg($pyannote_script),
-        $file,
-        escapeshellarg($output_file)
-    );
-    system($cmd, $ret);
-
-    $result = new StdClass;
-    $result->result = json_decode(file_get_contents($output_file));
-
-    JobHelper::updateData($job_id, 'result', $result);
-    JobHelper::updateStatus($job_id, 'done');
-} else {
-    $logger("unknown tool: " . $job->tool);
+} catch (Exception $e) {
+    $logger("error: " . $e->getMessage());
     JobHelper::updateStatus($job_id, 'error');
     exit;
 }
