715ce47bd0e3004fa193c529c7700ef9d7f926cd
Ronny Wang <ronnywang@gmail.com>
2025-04-24 14:40:09 +0800

    增加上傳上限

--- init.inc.php
+++ init.inc.php
@@ -2,6 +2,8 @@
 
 // timezone to Asia/Taipei
 date_default_timezone_set('Asia/Taipei');
+ini_set('post_max_size', '1024M');
+ini_set('upload_max_filesize', '1024M');
 
 if (file_exists(__DIR__ . '/config.php')) {
     include(__DIR__ . '/config.php');
